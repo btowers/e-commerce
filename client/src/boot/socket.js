66c527598b9f9052c27c85652709e05bdf2394d6
brako2 <49849533+brako2@users.noreply.github.com>
2022-01-04 00:37:51 -0300

    process.env client

--- client/src/boot/socket.js
+++ client/src/boot/socket.js
@@ -1,7 +1,6 @@
 import { boot } from 'quasar/wrappers';
 import { io } from 'socket.io-client';
 
-console.log('ENV: ', process.env.ENV);
 const URL =
   process.env.ENV == 'localhost'
     ? 'localhost:8080'
